2ed88887234632195df27a03398707239e6d4dfd
JamesTadleyGreen <JamesTadleyGreen@outlook.com>
2024-04-28 12:30:55 +0100

    feat: moving LSP to it's own file

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,9 +1,15 @@
 return {
-	"iamcco/markdown-preview.nvim",
 	{
 		"tpope/vim-fugitive",
 		config = function()
 			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		config = function()
+			vim.fn["mkdp#util#install"]()
+			vim.keymap.set("n", "<leader>m", ":MarkdownPreview<CR>")
 		end,
 	},
 }
